af0a003815e9f03dd44504c6b949af5542f74871
vedant202 <vdhenge202@gmail.com>
2023-07-11 16:56:40 +0530

    env file is added

--- server/database/db.js
+++ server/database/db.js
@@ -1,7 +1,10 @@
 import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 const Connection = ()=>{
-    const DB_URL = `mongodb://vedantdhenge9:vedantgmailclone@ac-gx2nodh-shard-00-00.6qtbtyp.mongodb.net:27017,ac-gx2nodh-shard-00-01.6qtbtyp.mongodb.net:27017,ac-gx2nodh-shard-00-02.6qtbtyp.mongodb.net:27017/?ssl=true&replicaSet=atlas-nvu0ta-shard-0&authSource=admin&retryWrites=true&w=majority`
+    const DB_URL = `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@ac-gx2nodh-shard-00-00.6qtbtyp.mongodb.net:27017,ac-gx2nodh-shard-00-01.6qtbtyp.mongodb.net:27017,ac-gx2nodh-shard-00-02.6qtbtyp.mongodb.net:27017/?ssl=true&replicaSet=atlas-nvu0ta-shard-0&authSource=admin&retryWrites=true&w=majority`
     try {
         mongoose.connect(DB_URL,{useNewUrlParser:true});
         console.log("DB Connected Successfully");
